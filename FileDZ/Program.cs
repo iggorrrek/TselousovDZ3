05727f725bfee19519af3730f9ee7944027acda3
iggorrrek <igor_tselousov@mail.ru>
2024-11-20 00:33:20 +0300

    готовое

--- FileDZ/Program.cs
+++ FileDZ/Program.cs
@@ -1,15 +1,149 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace FileDZ
 {
     internal class Program
     {
+        enum Week
+        {
+            Понедельник,
+            Вторник,
+            Среда,
+            Четверг,
+            Пятница,
+            Суббота,
+            Воскресенье
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Задание 1\n");
+
+            Console.WriteLine("Введите возрастающую последовательность из 10 чисел");
+            int[] numbers = new int[10];
+            Console.WriteLine("");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Введите число: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (numbers[i+1] < numbers[i])
+                {
+                    Console.WriteLine("Неверная последовательность!!!");
+                    break;
+                }
+            }
+            Console.WriteLine("\nЗадание 2\n");
+
+            try
+            {
+                Console.Write("Введите номер карты (6-14): ");
+                Byte k = Byte.Parse(Console.ReadLine());
+                string cardName;
+                switch (k)
+                {
+                    case 6:
+                        cardName = "Шестерка";
+                        break;
+                    case 7:
+                        cardName = "Семерка";
+                        break;
+                    case 8:
+                        cardName = "Восьмерка";
+                        break;
+                    case 9:
+                        cardName = "Девятка";
+                        break;
+                    case 10:
+                        cardName = "Десятка";
+                        break;
+                    case 11:
+                        cardName = "Валет";
+                        break;
+                    case 12:
+                        cardName = "Дама";
+                        break;
+                    case 13:
+                        cardName = "Король";
+                        break;
+                    case 14:
+                        cardName = "Туз";
+                        break;
+                    default:
+                        cardName = "Ошибка"; 
+                        break;
+                }
+
+                Console.WriteLine($"Это карта: {cardName}");
+            }
+            
+            finally
+            {
+                Console.WriteLine("Программа завершена.");
+            }
+
+            Console.WriteLine("\nЗадание 3\n");
+
+            Console.WriteLine("Введите на английском языке общность людей чтобы узнать что они предпочитают пить");
+            string input = Console.ReadLine();
+            string nuznoe = input.ToLower();
+
+            try
+            {
+                string result = GetOutput(nuznoe);
+                    
+                Console.WriteLine($"Нужно пить: {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"мммм: {ex.Message}");
+            }
+
+            Console.WriteLine("\nЗадание 4\n");
+
+            Console.WriteLine("Введите порядковое число дня недели (1-7):");
+            Byte den = Byte.Parse(Console.ReadLine());
+            var dayName = Enum.GetName(typeof(Week), den - 1);
+            Console.WriteLine($"Название дня недели: {dayName}");
+
+            Console.WriteLine("\nЗадание 5\n");
+
+            string[] toys = { "Hello Kitty", "Barbie doll", "Lego", "Transformers", "Hello Kitty", "Superman", "Barbie doll" };
+            int cnt = 0;
+
+            foreach (string toy in toys)
+            {
+                if (toy == "Hello Kitty" || toy == "Barbie doll")
+                {
+                    cnt++;
+                }
+            }
+
+            Console.WriteLine($"Количество кукол в сумке: {cnt}");
+
+            Console.ReadKey();
         }
+        static string GetOutput(string input)
+    {
+        switch (input)
+        {
+            case "jabroni":
+                return "Patron Tequila";
+            case "school counselor":
+                return "Anything with Alcohol";
+            case "programmer":
+                return "Hipster Craft Beer";
+            case "bike gang member":
+                return "MoonShine";
+            case "politician":
+                return "Your tax dollars";
+            case "rapper":
+                return "Cristal";
+            default:
+                return "Beer";
+        }
+    }
     }
 }
---
+++ TumakovDZ/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TumakovDZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 4.1, 4.2\n"); //просто получидлось так что сделал сразу с проверкой числа не читая 4.2
+
+            Console.WriteLine("Введите номер дня в году (от 1 до 365):");
+            Int16 vvedeno = Int16.Parse(Console.ReadLine());
+            if (vvedeno < 1 || vvedeno > 365 || vvedeno == 0)
+            {
+                Console.WriteLine("Неверно!!!!!, давай по новой");
+                vvedeno = Int16.Parse(Console.ReadLine());
+            }
+            Byte[] daysVmes = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            string[] monthNames = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+            int denVmes = vvedeno;
+            int nomMes = 0;
+            while (denVmes > daysVmes[nomMes])
+            {
+                denVmes -= daysVmes[nomMes];
+                nomMes++;
+            }
+            Console.WriteLine($"Это {denVmes} {monthNames[nomMes]}");
+
+            Console.WriteLine("\nДЗ 4.1\n");
+
+            Console.WriteLine("Введите год:");
+            Int16 god = Int16.Parse(Console.ReadLine());
+            if ((god % 400 ) == 0)
+            {
+                Console.WriteLine("Високосный");
+            }
+            else if ((god % 4) == 0 & (god % 100) != 0)
+            {
+                Console.WriteLine("Високосный");
+            }
+            else
+            {
+                Console.WriteLine("Не високосный");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
